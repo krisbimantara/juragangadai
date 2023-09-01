9c0e7462c0ce79f73608bd8d5110a2fb14319c34
Kristoforus Bimantara <74865512+krisbimantara@users.noreply.github.com>
2023-09-01 10:24:02 +0700

    Update moduleWeb.py

--- modules/moduleWeb.py
+++ modules/moduleWeb.py
@@ -1,8 +1,13 @@
-import locale
-
-def rupiah_format(angka, with_prefix=False, desimal=2):
-    locale.setlocale(locale.LC_NUMERIC, 'IND')
-    rupiah = locale.format("%.*f", (desimal, angka), True)
-    if with_prefix:
-        return "Rp. {}".format(rupiah)
-    return rupiah
+def rupiah_format(angka):
+    rev1 = str(angka)
+    rev = rev1[::-1]
+    rev2= ''
+    i=0
+    while i < len(rev):
+        rev2 += rev[i]
+        if (i+1) % 3 == 0 and i != (len(rev)-1) and rev[i] !='.':
+            rev2 += '.'
+        if rev[i]=='.':
+            rev2 += ';,'
+        i+=1
+    return 'Rp. ' + rev2[::-1].replace(';.','')
