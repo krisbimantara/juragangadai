926d8135ed4d444c2df803245c3f0abe61d718a8
Kristoforus Bimantara <74865512+krisbimantara@users.noreply.github.com>
2023-09-01 10:24:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,17 +16,17 @@ def endUserMokas():
         pinjam = request.form.get("pinjam")
         tenor1 = int(tenor)
         pinjam1 = int(pinjam.replace(".",""))
-        pinjam11= mw.rupiah_format(pinjam1,True)
+        pinjam11= mw.rupiah_format(pinjam1)
         pokok = round(pinjam1/tenor1,2)
-        pokok1 = mw.rupiah_format(pokok,True)
+        pokok1 = mw.rupiah_format(pokok)
         angs = data[(data['Tenor'] == tenor1) & (data['Pinjam']==pinjam1)].index
         try:
             angs2 = data['Angsuran'].iloc[angs].values[0]
-            angs22 = mw.rupiah_format(angs2,True)
+            angs22 = mw.rupiah_format(angs2)
             bunga = angs2-pokok
-            bunga1 = mw.rupiah_format(bunga,True)
+            bunga1 = mw.rupiah_format(bunga)
             totalAngs = angs2*tenor1
-            totalAngs1 = mw.rupiah_format(totalAngs, True)
+            totalAngs1 = mw.rupiah_format(totalAngs)
             return render_template('doneMuncul.html', datakons={'tenor':tenor ,'pinjam': pinjam11, 'pokok':pokok1, 'angs':angs22, 'bunga':bunga1, 'totalAngs':totalAngs1})
         except:
             return render_template('doneNmuncl.html', error_text={'err1':'Nilai Angsuran Tidak Ditemukan', 'err2':'Silahkan coba lagi.'})
