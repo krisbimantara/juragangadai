e768ea2f067e772d5e5c155894050b25a17b9eb2
Kristoforus Bimantara <74865512+krisbimantara@users.noreply.github.com>
2023-09-01 08:59:04 +0700

    Delete handlers/routes.py

--- handlers/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template
-from modules import modules
-
-def configure_routes(app):
-    @app.route("/")
-    def index():
-        hello = modules.hello()
-        content = modules.content()
-        return render_template("index.html", hello=hello, content=content)
